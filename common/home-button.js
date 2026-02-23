ac03eee3b5ea9665ea826c7e6ddedaee8cc137ac
baekchanghyun <changhyun@mement.net>
2026-02-23 18:36:37 +0900

    Revert "[COMMON] 한글 단어 단위 줄바꿈 적용 (word-break: keep-all)"
    This reverts commit 41ba56792d94c076457ce6a40eb2aec48bd5c011.

--- common/home-button.js
+++ common/home-button.js
@@ -15,7 +15,6 @@
   // 2. CSS 주입
   var style = document.createElement('style');
   style.textContent = [
-    'html { word-break: keep-all; }',
     '.ptv-home-btn {',
     '  position: fixed;',
     '  left: 16px;',
